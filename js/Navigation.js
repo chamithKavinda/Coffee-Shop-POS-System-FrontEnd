45fcdc913c04a895046c73f325307c65e43c69ee
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-29 11:38:15 +0530

    navigation done

---
+++ js/Navigation.js
@@ -0,0 +1,10 @@
+document.getElementById("Dashboard-button").addEventListener("click", function () {
+    document.getElementById("CustomerForm").style.display = "none";
+    document.getElementById("DashboardForm").style.display = "block";
+  });
+
+  document.getElementById("CustomerForm-button").addEventListener("click", function () {
+    document.getElementById("CustomerForm").style.display = "block";
+    document.getElementById("DashboardForm").style.display = "none";
+  });
+  
