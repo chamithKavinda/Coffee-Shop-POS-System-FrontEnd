2b8bcd6136c4e5df0a8578f483e49b89db216a31
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-30 14:37:58 +0530

    added navigation for Product form

--- js/Navigation.js
+++ js/Navigation.js
@@ -7,7 +7,7 @@ document.getElementById("Dashboard-button").addEventListener("click", function (
   document.getElementById("CustomerForm-button").addEventListener("click", function () {
     document.getElementById("CustomerForm").style.display = "block";
     document.getElementById("DashboardForm").style.display = "none";
-    document.getElementById("ProductsForm").style.display = "block";
+    document.getElementById("ProductsForm").style.display = "none";
   });
 
   document.getElementById("ProductsForm-button").addEventListener("click", function () {
