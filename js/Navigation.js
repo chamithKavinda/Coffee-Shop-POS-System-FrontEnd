aa3481cc13cf36585af18d7ce036e87c981faefc
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-30 23:34:20 +0530

    Navigation done for Orders Form

--- js/Navigation.js
+++ js/Navigation.js
@@ -2,17 +2,27 @@ document.getElementById("Dashboard-button").addEventListener("click", function (
     document.getElementById("CustomerForm").style.display = "none";
     document.getElementById("DashboardForm").style.display = "block";
     document.getElementById("ProductsForm").style.display = "none";
-  });
+    document.getElementById("OrdersForm").style.display = "none";
+});
 
-  document.getElementById("CustomerForm-button").addEventListener("click", function () {
+document.getElementById("CustomerForm-button").addEventListener("click", function () {
     document.getElementById("CustomerForm").style.display = "block";
     document.getElementById("DashboardForm").style.display = "none";
     document.getElementById("ProductsForm").style.display = "none";
-  });
+    document.getElementById("OrdersForm").style.display = "none";
+});
 
-  document.getElementById("ProductsForm-button").addEventListener("click", function () {
+document.getElementById("ProductsForm-button").addEventListener("click", function () {
     document.getElementById("CustomerForm").style.display = "none";
     document.getElementById("DashboardForm").style.display = "none";
     document.getElementById("ProductsForm").style.display = "block";
-  });
+    document.getElementById("OrdersForm").style.display = "none";
+});
+
+document.getElementById("OrdersForm-button").addEventListener("click", function () {
+  document.getElementById("CustomerForm").style.display = "none";
+  document.getElementById("DashboardForm").style.display = "none";
+  document.getElementById("ProductsForm").style.display = "none";
+  document.getElementById("OrdersForm").style.display = "block";
+});
   
