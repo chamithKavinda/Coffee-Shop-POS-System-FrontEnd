9e3174c640512b48e98d0eb363d5b125d57bb226
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-30 13:37:42 +0530

    Products form navigation implement

--- js/Navigation.js
+++ js/Navigation.js
@@ -1,10 +1,18 @@
 document.getElementById("Dashboard-button").addEventListener("click", function () {
     document.getElementById("CustomerForm").style.display = "none";
     document.getElementById("DashboardForm").style.display = "block";
+    document.getElementById("ProductsForm").style.display = "none";
   });
 
   document.getElementById("CustomerForm-button").addEventListener("click", function () {
     document.getElementById("CustomerForm").style.display = "block";
     document.getElementById("DashboardForm").style.display = "none";
+    document.getElementById("ProductsForm").style.display = "block";
+  });
+
+  document.getElementById("ProductsForm-button").addEventListener("click", function () {
+    document.getElementById("CustomerForm").style.display = "none";
+    document.getElementById("DashboardForm").style.display = "none";
+    document.getElementById("ProductsForm").style.display = "block";
   });
   
